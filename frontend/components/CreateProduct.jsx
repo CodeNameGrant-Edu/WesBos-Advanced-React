9998cbd0a99ba9bb45fdd6275f6f3ace3f09b39b
Grant Walker <GrantW2505@gmail.com>
2023-01-18 20:54:46 +0200

    23-Create Form and added form styles

--- frontend/components/CreateProduct.jsx
+++ frontend/components/CreateProduct.jsx
@@ -5,37 +5,50 @@ export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm();
 
   return (
-    <form action="">
-      <label htmlFor="name">
-        Name:
-        <input
-          value={inputs.name}
-          onChange={handleChange}
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Name"
-        />
-      </label>
+    <Form onSubmit={(e) => e.preventDefault()}>
+      <fieldset>
+        <label htmlFor="image">
+          Image:
+          <input onChange={handleChange} type="file" id="image" name="image" />
+        </label>
 
-      <label htmlFor="price">
-        Price:
-        <input
-          value={inputs.price}
-          onChange={handleChange}
-          type="number"
-          id="price"
-          name="price"
-          placeholder="Price"
-        />
-      </label>
+        <label htmlFor="name">
+          Name:
+          <input
+            value={inputs.name}
+            onChange={handleChange}
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name"
+          />
+        </label>
 
-      <button type="button" onClick={clearForm}>
-        Clear Form
-      </button>
-      <button type="button" onClick={resetForm}>
-        Reset Form
-      </button>
-    </form>
+        <label htmlFor="price">
+          Price:
+          <input
+            value={inputs.price}
+            onChange={handleChange}
+            type="number"
+            id="price"
+            name="price"
+            placeholder="Price"
+          />
+        </label>
+
+        <label htmlFor="description">
+          Description:
+          <textarea
+            value={inputs.description}
+            onChange={handleChange}
+            id="description"
+            name="description"
+            placeholder="Description"
+          />
+        </label>
+
+        <button type="submit">+ Add Product</button>
+      </fieldset>
+    </Form>
   );
 }
--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -11,7 +11,7 @@ export default function useForm(initial = {}) {
     }
 
     if (type === 'file') {
-      value[0] = e.target.value;
+      [value] = e.target.value;
     }
 
     setInputs({ ...inputs, [name]: value });
